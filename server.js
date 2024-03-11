10c9179bb2e184c6e8c11566454539d15afa0b10
Lukheo <theolucas200272@gmail.com>
2024-03-11 16:27:53 +0100

    add app listening at port

--- server.js
+++ server.js
@@ -13,3 +13,27 @@ const config = require('./config')
 
 const app = express()
 const port = 3000
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.use('/', router)
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at 127.0.0.1:${port}`)
+})
