aa8339ec3445d0f91db3bae4460bfcd7468b1456
Joeyybad <jnkunga@hotmail.com>
2024-03-12 15:22:12 +0100

    modif lien navbar inscription

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 const { Op } = require('sequelize');
 const bcrypt = require("bcrypt");
 const { validationResult } = require('express-validator');
@@ -10,7 +5,7 @@ const { Request, Response } = require('express');
 const User = require('../models/userModel')
 
 module.exports = {
-  list: async (req,res) => {
+    list: async (req,res) => {
 
     },
     get: (req, res) => { //
