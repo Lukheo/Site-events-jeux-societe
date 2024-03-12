e77a3d4f87686e5f91a972b877c458b55b07b271
Lukheo <theolucas200272@gmail.com>
2024-03-12 13:27:49 +0100

    add event, game and event models, event controller and start event_list page

---
+++ api/models/eventModel.js
@@ -0,0 +1,27 @@
+const { DataTypes} = require('sequelize')
+const bcrypt = require('bcrypt')
+const config = require('../../config')
+const Game = require('./gameModel')
+const Categorie = require('./categorieModel')
+
+
+const Event = config.sequelize.define('events', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    event_date: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    players_number: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+})
+
+Event.hasOne(Game)
+Event.hasMany(Categorie)
+
+module.exports = Event
--- api/router.js
+++ api/router.js
@@ -14,6 +14,7 @@ router.route('/')
 
 
 router.route('/events/list')
+    .get(eventController.list)
 
 
 
